302385d4f1571e1e7b382af36838f809e499a001
Yabets solomon <jabezsolomonz@gmail.com>
2025-03-04 03:35:30 +0300

    added these ones

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,3 @@
-import daisyui from "daisyui"
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,5 +7,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
-};
+  plugins: [],
+}
